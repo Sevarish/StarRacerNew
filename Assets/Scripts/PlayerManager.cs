d08be51ce699a892dd672e2f5d3ade7af3a21b62
DESKTOP-GDM5KI7\Mreow <Sevarish@hotmail.com>
2018-05-31 09:20:50 +0200

    no message

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+    public float shieldTimer = 0.0f;
+    public bool isShielded = false;
+    bool doOnce = true;
+    bool isHitFinal = false;
+    int count = 0;
+    int explosionTimer = 0;
+    bool explosionActive = false;
+    public int lives = 3;
+    public SpriteRenderer render;
+    public Sprite shielded;
+    public Sprite normal;
+    public Animator anim;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        anim.Play("null");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isShielded == true)
+        {
+            render.sprite = shielded;
+            anim.Play("shield");
+            
+            shieldTimer += Time.deltaTime;
+            if (shieldTimer > 5.5f && shieldTimer < 7 && doOnce == true)
+            {
+                GameObject.Find("Player").GetComponent<PlayerMovement>().speedUp -= 20;
+                doOnce = false;
+            }
+            if (shieldTimer >= 7)
+            {
+                
+                isShielded = false;
+                doOnce = true;
+                anim.Play("null");
+            }
+        }
+        else
+        {
+            render.sprite = normal;
+        }
+
+        
+
+
+        if (isHitFinal == true)
+        {
+            count++;
+            if (count == 30)
+            {
+                Destroy(this.gameObject);
+                Application.LoadLevel("DeathScreen");
+                isHitFinal = false;
+                count = 0;
+            }
+        }
+
+        if (explosionActive == true)
+        {
+            explosionTimer++;
+            if (explosionTimer == 25)
+            {
+                explosionTimer = 0;
+                explosionActive = false;
+                anim.Play("null");
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Obstacle" && isShielded == false && lives > 0)
+        {
+            lives--;
+            anim.Play("ExplosionAnim");
+            explosionActive = true;
+        }
+
+        if (collision.gameObject.tag == "Obstacle" && isShielded == false && lives <= 0)
+        {
+            isHitFinal = true;
+            anim.Play("ExplosionAnim");
+            explosionActive = true;
+        }
+
+
+    }
+
+    
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public Transform myT;
+    public float speedUp = 8;
+    public int speedHor = 9;
+    int maxHorRight = 9;
+    int maxHorLeft = -9;
+    int yPosition = 0;
+    public Vector3 resetPoint;
+    public Quaternion resetRotation;
+    public bool maxHeightReached = false;
+    // Use this for initialization
+    void Start () {
+        myT = transform;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        resetPoint.x = 0;
+        resetPoint.y = 10;
+        resetPoint.z = myT.position.z;
+
+        resetRotation.x = 0;
+        resetRotation.y = 0;
+        resetRotation.z = 0;
+
+
+
+
+
+        float x = Input.GetAxisRaw("Horizontal");
+
+        float y = Input.GetAxisRaw("Vertical");
+
+        if (myT.position.x <= maxHorRight && x < 0)
+        {
+            
+            transform.Translate(x * Time.deltaTime * -speedHor, 0, 0);
+        }
+        else if (myT.position.x >= maxHorLeft && x > 0)
+        {
+            
+            transform.Translate(x * Time.deltaTime * -speedHor, 0, 0);
+        }
+
+        if (y > 0 && yPosition <= 60)
+        {
+            float actualY = y / 8; 
+            yPosition++;
+            transform.Translate(0 ,actualY, 0);
+        }
+
+        if (y < 0 && yPosition >= -60)
+        {
+            float actualY = y / 8;
+            yPosition--;
+            transform.Translate(0, actualY, 0);
+        }
+
+        
+
+
+        if (maxHeightReached == true)
+        {
+            
+            speedUp += 0.8f;
+
+            if (GameObject.Find("Player").GetComponent<UI>().distanceCount < 2)
+            {
+                GameObject.Find("Player").GetComponent<UI>().multValue += 28000;
+            }
+            else
+            {
+                GameObject.Find("Player").GetComponent<UI>().multValue += 1500000;
+            }
+
+            maxHeightReached = false;
+        }
+
+        
+
+    }
+}
